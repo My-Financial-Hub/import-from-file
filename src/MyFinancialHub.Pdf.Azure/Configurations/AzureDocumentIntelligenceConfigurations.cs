d37ad92facf3a59d5615410710fbd19a0b65ee00
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-07 21:49:18 -0300

    Removes Pdf.Azure

--- src/MyFinancialHub.Pdf.Azure/Configurations/AzureDocumentIntelligenceConfigurations.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyFinancialHub.Pdf.Azure.Configurations
-{
-    internal class AzureDocumentIntelligenceConfigurations
-    {
-        public string Endpoint { get; init; } = string.Empty;
-        public string ApiKey { get; init; } = string.Empty;
-        public string ModelId { get; init; } = "prebuilt-layout"; // Default model ID
-    }
-}
--- src/MyFinancialHub.Pdf.Azure/Usings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Azure.AI.DocumentIntelligence;
-
-global using MyFinancialHub.Import.Core.Domain;
-global using MyFinancialHub.Import.Core.Interfaces.Repositories;
