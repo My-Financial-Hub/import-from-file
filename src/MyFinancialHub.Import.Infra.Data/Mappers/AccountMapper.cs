aa3d001433559e71f6297ca8c21edae1feac36eb
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-08 09:16:15 -0300

    Add AccountRepository

---
+++ src/MyFinancialHub.Import.Infra.Data/Mappers/AccountMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace MyFinancialHub.Import.Infra.Data.Mappers
+{
+    internal class AccountMapper(BalanceMapper balanceMapper)
+    {
+        private readonly BalanceMapper balanceMapper = balanceMapper;
+
+        public Account Map(AccountEntity entity)
+        {
+            if (entity == null)
+                return null;
+            var account = new Account(entity.Name, 0);
+            if(entity.Balances?.Count > 0)
+            {
+                account.AddBalances(entity.Balances.Select(balanceMapper.Map));
+            }
+
+            return account;
+        }
+
+        public AccountEntity Map(Account account)
+        {
+            if (account == null)
+                return null;
+
+            var entity = new AccountEntity
+            {
+                Name = account.Name,
+                Balances = account.Balances.Select(balanceMapper.Map).ToArray(),
+            };
+            return entity;
+        }
+    }
+}
---
+++ src/MyFinancialHub.Import.Infra.Data/Repositories/AccountsRepository.cs
@@ -0,0 +1,39 @@
+﻿using MyFinancialHub.Import.Infra.Data.Mappers;
+
+namespace MyFinancialHub.Import.Infra.Data.Repositories
+{
+    internal class AccountsRepository(FinancialHubContext context, AccountMapper mapper) : IAccountRepository
+    {
+        private readonly FinancialHubContext context = context;
+        private readonly AccountMapper mapper = mapper;
+
+        public async Task CreateAsync(Account account)
+        {
+            var entity = this.mapper.Map(account);
+
+            var now = DateTime.UtcNow;
+            entity.CreationTime = now;
+            entity.UpdateTime = now;
+
+            await this.context.Accounts.AddAsync(entity);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task<Account> GetByNameAsync(string name)
+        {
+            var account = await this.context.Accounts.FirstOrDefaultAsync(a => a.Name == name);
+            if (account == null)
+                return null;
+
+            return this.mapper.Map(account);
+        }
+
+        public async Task UpdateAsync(Account account)
+        {
+            var entity = this.mapper.Map(account);
+
+            this.context.Accounts.Update(entity);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
