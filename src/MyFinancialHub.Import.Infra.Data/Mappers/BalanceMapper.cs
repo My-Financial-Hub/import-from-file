fd2e20a905860c414b67d8b41c61928f17e8db99
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-08 09:20:30 -0300

    Adds MyFinancialHub.Import.Infra.Data

---
+++ src/MyFinancialHub.Import.Infra.Data/Mappers/BalanceMapper.cs
@@ -0,0 +1,27 @@
+﻿namespace MyFinancialHub.Import.Infra.Data.Mappers
+{
+    internal class BalanceMapper
+    {
+        public Balance Map(BalanceEntity entity)
+        {
+            if (entity == null)
+                return null;
+            var balance = new Balance(entity.Name, 0);
+            // balance.AddTransactions(entity.Transactions);
+
+            return balance;
+        }
+
+        public BalanceEntity Map(Balance balance)
+        {
+            if (balance == null)
+                return null;
+
+            var entity = new BalanceEntity
+            {
+                Name = balance.Name,
+            };
+            return entity;
+        }
+    }
+}
---
+++ src/MyFinancialHub.Import.Infra.Data/Usings.cs
@@ -0,0 +1,17 @@
+﻿// System namespaces
+global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+
+// 3rd party namespaces
+global using Microsoft.EntityFrameworkCore;
+
+// Project namespaces
+global using MyFinancialHub.Import.Infra.Data.Entities;
+global using MyFinancialHub.Import.Infra.Data.Contexts;
+global using MyFinancialHub.Import.Infra.Data.Repositories;
+global using MyFinancialHub.Import.Domain.Interfaces.Repositories;
+
+// Domain namespaces
+global using MyFinancialHub.Import.Domain.Entities;
+ 
