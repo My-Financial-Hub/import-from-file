04ed39f7a53a42ebf4122d5d2b89bb274c62990f
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-08 09:22:41 -0300

    Adds MyFinancialHub.Import.Console

--- src/MyFinancialHub.Import.Console/Program.cs
+++ src/MyFinancialHub.Import.Console/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using MyFinancialHub.Import.Infra.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MyFinancialHub.Application.CQRS;
+using MyFinancialHub.Import.Application;
+using MyFinancialHub.Import.Application.Handlers.ImportPdfFile;
+using MyFinancialHub.Import.Infra.AI.DocumentIntelligence;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddLogging(configure => configure.AddConsole());
+serviceCollection
+    .AddApplication()
+    .AddSqlRepositories()
+    .AddDocumentInteligence();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var service = serviceProvider.GetRequiredService<IDispatcher>();
+
+var Main = async () =>
+{
+    try
+    {
+        var path = "C:\\Users\\Frank\\Desktop\\2020_lancamentos.pdf";
+        var file = File.OpenRead(path);
+        var accountName = "TestAccount";
+        await service.Dispatch(new ImportPdfFileCommand(file, accountName));
+
+        Console.WriteLine("Document analysis completed successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+    }
+    Console.ReadKey();
+};
+
+Main().GetAwaiter().GetResult();
