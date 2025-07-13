570a7a0e1539ff6dd57bea77cf3785f3bafba85f
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-13 12:36:04 -0300

    Removes IBalanceService

--- src/MyFinancialHub.Import.Domain/Interfaces/Services/IBalanceImportService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MyFinancialHub.Import.Domain.Entities.Accounts;
-
-namespace MyFinancialHub.Import.Domain.Interfaces.Services
-{
-    public interface IBalanceImportService
-    {
-        Task<IEnumerable<Balance>> ImportAsync(Stream fileStream);
-    }
-}
